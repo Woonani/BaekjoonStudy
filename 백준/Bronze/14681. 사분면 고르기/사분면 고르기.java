b473e9f37df73d6a6ff74368e7d536ec4cedf584
Woonani <nada3377@naver.com>
2024-03-29 23:14:11 +0900

    [Bronze V] Title: 사분면 고르기, Time: 80 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -3,9 +3,11 @@ import java.util.*;
 class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // StringTokenizer st = new StringTokenizer(br.readLine());
-        int x = Integer.parseInt(br.readLine()); // Integer.parseInt(st.nextToken()); 
-        int y = Integer.parseInt(br.readLine()); // Integer.parseInt(st.nextToken()); 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        //Scanner sc = new Scanner(System.in);
+        int x = Integer.parseInt(st.nextToken()); //sc.nextInt();
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        int y = Integer.parseInt(st2.nextToken()); //sc.nextInt();
         if(x*y > 0){
             if (x>0){
                 System.out.println(1)   ; 
