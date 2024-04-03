aaa83fa16b5d6aa5fc95f6986109fefa5c512818
Woonani <nada3377@naver.com>
2024-04-03 14:56:22 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 12768 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main (String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        char x = sc.next().charAt(0);
+        int y = (int) x;
+        System.out.println(y);
+        
+    }
+}
